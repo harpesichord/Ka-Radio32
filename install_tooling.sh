d520741325224219b87a564c195fb060991c6267
Michael <michael@harpelife.com>
2022-02-04 22:31:14 -0500

    Added tooling support

---
+++ install_tooling.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Setup Tooling
+
+pip install --upgrade setuptools
+python -m pip install --upgrade pip
+pip install future
+pacman -S mingw-w64-i686-python2-cryptography
+pip install cryptography
+pip install -r $IDF_PATH/requirements.txt
