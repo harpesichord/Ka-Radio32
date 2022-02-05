e27fe629f2d228a84afc368a9dcfd7c9c1dd1453
Michael <michael@harpelife.com>
2022-02-04 23:12:30 -0500

    Added files for webpage generation

--- generate.sh
+++ generate.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-cd webpage
-./generate.sh
-cd ..
+
+make partition_table
+
+cd boards
+./nvs_partition_generator.sh lolin32pro.csv
+cd ../
+
+# Ran this in WSL on windows since can't get python3 installed anymore
+#cd webpage
+#./generate.sh
+#cd ..
+
 echo "make flash"
-make app
+make -j12 app
 
